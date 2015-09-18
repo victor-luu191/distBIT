bae38315388ba910d540f77e0ea220364a04e4f1
minhduc <musketeer191@gmail.com>
2015-09-18 11:27:07 +0800

    filled in details of converters, not tested yet

--- main/java/utils/Converters.java
+++ main/java/utils/Converters.java
@@ -1,28 +1,37 @@
 package utils;
 
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealVector;
 import org.petuum.jbosen.PsTableGroup;
-import org.petuum.jbosen.row.Row;
 import org.petuum.jbosen.row.double_.DenseDoubleRowUpdate;
+import org.petuum.jbosen.row.double_.DoubleColumnIterator;
+import org.petuum.jbosen.row.double_.DoubleRow;
 import org.petuum.jbosen.row.double_.DoubleRowUpdate;
 import org.petuum.jbosen.table.DoubleTable;
 
 public class Converters {
 
-	public static RealVector toVector(Row row) {
+	public static RealVector toVector(DoubleRow row) {
 		// TODO Auto-generated method stub
-		return null;
+		RealVector v = new ArrayRealVector();
+		DoubleColumnIterator iter = row.iterator();
+		iter.advance();
+		while (iter.hasNext()) {
+			v.append(iter.getValue());
+		}
+		
+		return v;
 	}
 
 	public static RealVector arr2Vector(double[] arr) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new ArrayRealVector(arr);
 	}
 
 	public static DoubleTable toTable(double[][] arr, int tableId) {
 		int staleness = 0;
 		int numCol = arr[0].length;
-		// TODO Auto-generated method stub
+		
 		PsTableGroup.createDenseDoubleTable(tableId, staleness, numCol);
 		DoubleTable table = PsTableGroup.getDoubleTable(tableId);
 		int numRow = arr.length;
