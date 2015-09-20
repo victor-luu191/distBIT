b6df695c86853f77af5600e11b647d90115876d0
minhduc <musketeer191@gmail.com>
2015-09-20 18:12:00 +0800

    corrected decPairCount() in CountTables class

--- main/java/defs/CountTables.java
+++ main/java/defs/CountTables.java
@@ -49,19 +49,23 @@ public class CountTables {
 	
 	public void decPairCount(Pair cPair, int userIndex, int itemIndex, int cTopic) {
 		
-		decisionUser.inc(2, userIndex, -1);	// decrease marginal decision counts
 		if (cPair.getDecision() == false) {// currently no brand is used
 			decisionUser.inc(0, userIndex,  -1);
-			itemTopic.inc(cTopic, itemIndex, -1);
-			sumItem4Topic[cTopic]--;
+			decisionUser.inc(2, userIndex, -1);	// decrease marginal decision counts
+			
+			itemTopic.inc(itemIndex, cTopic,  -1);
+			itemTopic.inc(dims.numItem, cTopic, -1);
 		} 
 		else {//currently brand is used
 			decisionUser.inc(1, userIndex,  -1);
+			decisionUser.inc(2, userIndex, -1);	// decrease marginal decision counts
+			
 			int cBrandIndex = cPair.getBrandIndex();
-			brandTopic.inc(cTopic, cBrandIndex, -1);
-			sumBrand4Topic[cTopic]--;
-			itemBrand.inc(cBrandIndex, itemIndex, -1);
-			marginCountOfBrand[cBrandIndex]--;
+			brandTopic.inc(cBrandIndex, cTopic,  -1);
+			brandTopic.inc(dims.numBrand, cTopic, -1);
+			
+			itemBrand.inc(itemIndex, cBrandIndex,  -1);
+			itemBrand.inc(dims.numItem, cBrandIndex, -1);
 		}
 	}
 	
