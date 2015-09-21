fad1e8f860a4a94aabfb4b72b6e450b65e5ebdf0
minhduc <musketeer191@gmail.com>
2015-09-21 17:47:42 +0800

    Bf change data struct of class Assignments to Table struct in petuum

--- main/java/bit/BrandItemTopicWorker.java
+++ main/java/bit/BrandItemTopicWorker.java
@@ -3,16 +3,10 @@ package bit;
 import java.util.ArrayList;
 import java.util.Random;
 
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.RealMatrix;
 import org.petuum.jbosen.PsTableGroup;
-import org.petuum.jbosen.table.DoubleTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import utils.Converters;
-import utils.Stats;
-import defs.Assignments;
 import defs.CountTables;
 import defs.Dimensions;
 import defs.Instance;
@@ -40,7 +34,7 @@ public class BrandItemTopicWorker implements Runnable {
 	Random random;
 
 	public void run() {
-		// TODO Auto-generated method stub
+		// TODO 
 		countTables.topicUser = PsTableGroup.getDoubleTable(topicUserTableId);
 		countTables.decisionUser = PsTableGroup.getDoubleTable(decisionUserTableId);
 		countTables.itemTopic = PsTableGroup.getDoubleTable(itemTopicTableId);
@@ -51,7 +45,12 @@ public class BrandItemTopicWorker implements Runnable {
 		int userBegin = workerRank * numUserPerWorker;
 		int userEnd = (workerRank == numWorkers - 1)? dims.numUser : (userBegin + numUserPerWorker);
 		
+		// Since each thread initialize part of count tables, use barrier to
+        // ensure initialization completes.
 		initTables(countTables, userBegin, userEnd);
+		PsTableGroup.globalBarrier();
+		
+		
 	}
 	/**
 	 * Initialize a partition of count tables, this partition corresponds to users from {@code userBegin} 
